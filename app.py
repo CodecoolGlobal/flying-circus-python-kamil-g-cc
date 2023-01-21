9096a59c105ddbb82e32dcacb8ba14dfbe5559d5
Kamil Grondys <kamil.grondys@codecool.com>
2023-01-21 15:32:05 +0100

    adding session

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, session
 
 app = Flask(__name__)
+app.secret_key = "moj sekretny klucz"
 
 @app.route('/')
 def index():
-    response = make_response(render_template("index.html"))
+
+    ile_razy = session.get('ktory_raz_jestem', default=1)
+    session['ktory_raz_jestem'] = ile_razy + 1
+
+    response = make_response(render_template("index.html", visits = ile_razy))
+    response.set_cookie('kamil_ciasteczko', value="bylem tutaj, Tony Halik")
+
     return response
